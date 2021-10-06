373c8f3ddf76a22f723244b0c07b7e9475a88be6
Tian Fang <tianfangsg@gmail.com>
2021-10-06 12:39:43 +0800

    Add ionic

--- frontend/wottlenft/src/pages/_app.tsx
+++ frontend/wottlenft/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 
 import '../styles/main.css';
+import '@ionic/react/css/core.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
