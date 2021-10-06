f4f58a415e0140fd92cfb07d91081f34d59e7e20
Tian Fang <tianfangsg@gmail.com>
2021-10-06 20:11:35 +0800

    Change about layout

--- frontend/wottlenft/src/pages/about.tsx
+++ frontend/wottlenft/src/pages/about.tsx
@@ -1,23 +1,29 @@
-import { IonLabel } from '@ionic/react';
+import { IonContent, IonLabel } from '@ionic/react';
 
 import { Meta } from '../layout/Meta';
 import { Main } from '../templates/Main';
 
 const About = () => (
   <Main meta={<Meta title="About" description="Wottlenft About" />}>
-    <IonLabel>Wottlenft</IonLabel>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
-      voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
-      voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
-    </p>
+    <IonContent className="text-center">
+      <IonLabel color="primary" className="text-5xl">
+        Wottlenft
+      </IonLabel>
+      <div className="p-12 flex flex-col gap-5">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
+          recusandae quidem. Quaerat molestiae blanditiis doloremque possimus
+          labore voluptatibus distinctio recusandae autem esse explicabo
+          molestias officia placeat, accusamus aut saepe.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
+          recusandae quidem. Quaerat molestiae blanditiis doloremque possimus
+          labore voluptatibus distinctio recusandae autem esse explicabo
+          molestias officia placeat, accusamus aut saepe.
+        </p>
+      </div>
+    </IonContent>
   </Main>
 );
 
