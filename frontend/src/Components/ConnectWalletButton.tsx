708cdb4c3cb26d443d276463c48373eb4be7adff
tankangliang <tankangliang98@gmail.com>
2021-10-07 21:55:55 +0800

    lint files

--- frontend/src/Components/ConnectWalletButton.tsx
+++ frontend/src/Components/ConnectWalletButton.tsx
@@ -1,36 +1,44 @@
-import { useDispatch } from "react-redux";
-import { setWalletInfo } from "../features/wallet/walletSlice";
-import { storeWalletInfo } from "../lib/namiWallet";
+import { useDispatch } from 'react-redux';
+
+import { setWalletInfo } from '../features/wallet/walletSlice';
+import { storeWalletInfo } from '../lib/namiWallet';
 
 const ConnectWalletButton: React.FC = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const handleConnectWallet = () => {
-        var cardano = window.cardano;
-        // Check if there is a cardano provider
-        if (!cardano) {
-            alert("Cardano provider not detected! Get the Nami Wallet extension first.")
-            return;
-        }
-
-        cardano.enable().then(() => {
-            storeWalletInfo();
-            dispatch(setWalletInfo({
-                balance: 123,
-                address: "laskdjklsjd",
-            }))
-        }).catch((err: Error) => {
-            console.log(err);
-        });
+  const handleConnectWallet = () => {
+    const { cardano } = window;
+    // Check if there is a cardano provider
+    if (!cardano) {
+      alert(
+        'Cardano provider not detected! Get the Nami Wallet extension first.'
+      );
+      return;
     }
 
-    return (
-        <button 
-            className="h-16 bg-black text-white w-48 self-end rounded-full m-5"
-            onClick={handleConnectWallet}
-        >
-            Connect to wallet
-        </button>
-    )
-}
-export default ConnectWalletButton
+    cardano
+      .enable()
+      .then(() => {
+        storeWalletInfo();
+        dispatch(
+          setWalletInfo({
+            balance: 123,
+            address: 'laskdjklsjd',
+          })
+        );
+      })
+      .catch((err: Error) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <button
+      className="h-16 bg-black text-white w-48 self-end rounded-full m-5"
+      onClick={handleConnectWallet}
+    >
+      Connect to wallet
+    </button>
+  );
+};
+export default ConnectWalletButton;
