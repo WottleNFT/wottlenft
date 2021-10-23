f03999b0ba98591c5630f3ce99eac6c2ab84abdb
dominiclet <dominic.let@hotmail.com>
2021-10-23 23:44:41 +0800

    Add contribution table

---
+++ frontend/src/Components/Profile/Contributions.tsx
@@ -0,0 +1,65 @@
+import { IonButton } from "@ionic/react";
+import React from "react";
+import { Contribution } from "../../types/Contribution";
+
+const contributionList: Contribution[] = [{
+	date: "12/12/12",
+	hasContributed: true,
+	unGoal: "zero hunger",
+	nftPurchased: "egg man",
+	amountContributed: 1234,
+	id: "12345678",
+}, {
+	date: "12/12/12",
+	hasContributed: false,
+	nftPurchased: "egg woman",
+	id: "12341241231",
+}, {
+	date: "12/12/12",
+	hasContributed: true,
+	unGoal: "zero hunger",
+	nftPurchased: "egg man",
+	amountContributed: 1234,
+	id: "12345678",
+}];
+
+const Contributions = () => {
+	return (
+		<table className="w-full text-lg">
+			<thead>
+				<tr className="text-left border-b-2 border-black border-solid h-14">
+					<th className="px-2">Date</th>
+					<th className="px-2">Message</th>
+					<th className="px-2">Amount</th>
+					<th className="px-2">ID</th>
+				</tr>
+			</thead>
+			<tbody>
+				{contributionList.map(contributionInfo => 
+					<tr className="h-14">
+						<td className="px-2">
+							{contributionInfo.date}
+						</td>
+						<td className="px-2">
+							You have contributed to {" "}
+							<span className="text-primary-default">
+								{contributionInfo.hasContributed ? contributionInfo.unGoal : "_______"}
+							</span>
+							, from  purchasing {" "}
+							<span className="text-primary-default">
+								{contributionInfo.nftPurchased}
+							</span>
+						</td>
+						<td className="px-2 text-primary-default">
+							{contributionInfo.hasContributed ? `₳${contributionInfo.amountContributed}` : <IonButton className="rounded-2xl">Contribute</IonButton>}
+						</td>
+						<td className="px-2">
+							{contributionInfo.id}
+						</td>
+					</tr>
+				)}
+			</tbody>
+		</table>
+	);
+}
+export default Contributions
--- frontend/src/Components/Profile/Tabs.tsx
+++ frontend/src/Components/Profile/Tabs.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Contributions from "./Contributions";
 
 enum Tab {
 	Collection,
@@ -10,12 +11,13 @@ const Tabs = () => {
 	const [activeTab, setActiveTab] = useState<number>(Tab.Contribution)
 
 	return(
-		<div className="self-start my-20 ml-6">
-			<div className="flex">
+		<div className="self-start w-full my-20 ml-6">
+			<div className="flex my-5">
 					<button className={`px-5 text-2xl font-bold text-center border-r-2 border-black border-solid hover:text-primary-default ${activeTab === Tab.Collection && "text-primary-default"}`}>Collection</button>
 					<button className={`px-5 text-2xl font-bold text-center border-r-2 border-black border-solid hover:text-primary-default ${activeTab === Tab.Activity && "text-primary-default"}`}>Activity</button>
 					<button className={`px-5 text-2xl font-bold text-center hover:text-primary-default ${activeTab === Tab.Contribution && "text-primary-default"}`}>Contribution</button>
 			</div>
+			<Contributions />
 		</div>
 	);
 }
