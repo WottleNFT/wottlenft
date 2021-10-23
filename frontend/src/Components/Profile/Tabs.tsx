341265364c8c66d4a0c10393c315628e9170dcb1
dominiclet <dominic.let@hotmail.com>
2021-10-24 00:19:36 +0800

    Add animation for tab switching on profile

--- frontend/src/Components/Profile/Tabs.tsx
+++ frontend/src/Components/Profile/Tabs.tsx
@@ -1,5 +1,10 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import useWallet from "../../hooks/useWallet";
+import DisplayMessage from "../UserNfts/DisplayMessage";
+import NftList from "../UserNfts/NftList";
+import WalletSwitch from "../WalletSwitch";
 import Contributions from "./Contributions";
+import { CSSTransitionGroup } from 'react-transition-group';
 
 enum Tab {
 	Collection,
@@ -8,16 +13,35 @@ enum Tab {
 }
 
 const Tabs = () => {
+	const wallet = useWallet();
 	const [activeTab, setActiveTab] = useState<number>(Tab.Contribution)
 
 	return(
 		<div className="self-start w-full my-20 ml-6">
 			<div className="flex my-5">
-					<button className={`px-5 text-2xl font-bold text-center border-r-2 border-black border-solid hover:text-primary-default ${activeTab === Tab.Collection && "text-primary-default"}`}>Collection</button>
-					<button className={`px-5 text-2xl font-bold text-center border-r-2 border-black border-solid hover:text-primary-default ${activeTab === Tab.Activity && "text-primary-default"}`}>Activity</button>
-					<button className={`px-5 text-2xl font-bold text-center hover:text-primary-default ${activeTab === Tab.Contribution && "text-primary-default"}`}>Contribution</button>
+					<button onClick={() => setActiveTab(Tab.Collection)} className={`px-5 text-2xl font-bold text-center border-r-2 border-black border-solid hover:text-primary-default ${activeTab === Tab.Collection && "text-primary-default"}`}>Collection</button>
+					<button onClick={() => setActiveTab(Tab.Activity)} className={`px-5 text-2xl font-bold text-center border-r-2 border-black border-solid hover:text-primary-default ${activeTab === Tab.Activity && "text-primary-default"}`}>Activity</button>
+					<button onClick={() => setActiveTab(Tab.Contribution)} className={`px-5 text-2xl font-bold text-center hover:text-primary-default ${activeTab === Tab.Contribution && "text-primary-default"}`}>Contribution</button>
 			</div>
-			<Contributions />
+			<CSSTransitionGroup
+				transitionName="tab"
+				transitionEnterTimeout={300}
+				transitionLeaveTimeout={200}
+			>
+				{activeTab === Tab.Collection &&<WalletSwitch
+						wallet={wallet}
+						loading={<DisplayMessage text="Loading your wallet..." />}
+						notEnabled={<DisplayMessage text="Please enable Nami Wallet" />}
+						enabled={(enabledWallet) => (
+							<NftList
+								address={enabledWallet.state.address}
+								baseUrl={enabledWallet.state.backendApi}
+							/>
+						)}
+						fallback={<DisplayMessage text="Please get Nami Wallet" />}
+					/>}
+				{activeTab === Tab.Contribution && <Contributions />}
+			</CSSTransitionGroup>
 		</div>
 	);
 }
