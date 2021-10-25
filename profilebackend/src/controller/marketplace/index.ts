7f0a247ef0c5b419a0fe70869f8540741fec8262
pooty3 <aloysiuslim.dw@gmail.com>
2021-10-26 03:55:59 +0800

    modified listing schema added some api endpoints

---
+++ profilebackend/src/controller/marketplace/index.ts
@@ -0,0 +1,53 @@
+import * as express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { addNewListing, getAllListingsOfStatus, getListingByID } from '../../database/listingQueries'
+import { ListingStatus } from '../../models/listing'
+export async function getAllCurrentListings(req: express.Request, res: express.Response) {
+  try {
+    let listings = await getAllListingsOfStatus(ListingStatus.listing)
+    res.status(StatusCodes.OK).json({
+      listings: listings
+    })
+  } catch (error: any) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      errorMessage: error.message
+    })
+  }
+}
+export async function getListing(req: express.Request, res: express.Response) {
+  try {
+    let listing = await getListingByID(Number(req.params.listing_id))
+    if (listing == null) {
+      return res.status(StatusCodes.NOT_FOUND).json({
+        error: "No listing with ID"
+      })
+    }
+    return res.status(StatusCodes.OK).json({
+      listing: listing
+    })
+  } catch (error: any) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      errorMessage: error.message
+    })
+  }
+}
+export async function createListing(req: express.Request, res: express.Response) {
+  try{
+    await addNewListing({
+      listing_id: null,
+      nft_id: req.body.nft_id,
+      nft_asset_name: req.body.nft_asset_name,
+      buyer_wallet_id: null,
+      seller_wallet_id: req.body.wallet_id,
+      price: req.body.price,
+      current_status: ListingStatus.listing,
+      seller_contribution: null,
+      buyer_contribution: null,
+      un_goal: null
+    })
+  } catch (error: any) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      errorMessage: error.message
+    })
+  }
+}
--- profilebackend/src/database/listingQueries.ts
+++ profilebackend/src/database/listingQueries.ts
@@ -1,7 +1,21 @@
+import { QueryResult, QueryResultRow } from "pg";
+import { DataRowMessage } from "pg-protocol/dist/messages";
 import { Listing, ListingStatus } from "../models/listing";
 import pool from "./database";
 
 export async function getAllListingsOfStatus(status: ListingStatus): Promise<Listing[]> {
   let data = await pool.query("SELECT * FROM listings WHERE current_status = $1", [status as string])
   return data.rows
+}
+
+export async function getListingByID(id: number): Promise<Listing | null> {
+  let data = await pool.query("SELECT * FROM listings WHERE current_status = $1", [status as string])
+  return data.rows.length ==0 ? null : data.rows[0]
+}
+
+export async function addNewListing(listing: Listing): Promise<QueryResult> {
+  return pool.query("INSERT INTO listings(nft_id, nft_asset_name, seller_wallet_id, price," 
+    + "current_status, seller_contribution, un_goal) VALUES ($1,$2,$3,$4,$5,$6,$7",
+    [listing.nft_id, listing.nft_asset_name, listing.seller_wallet_id, listing.price, 
+      listing.current_status, listing.seller_contribution, listing.un_goal])
 }
