993f32250600abf53dd2d3158d9f198436ca0265
Elton <57287843+eltontay@users.noreply.github.com>
2021-10-25 21:20:34 +0800

    a bit more changes to about us

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -43,13 +43,13 @@ const About = () => {
     <p className="text-xl">
       WottleNFT will first focus on building an amazing marketplace. Afterwhich,
       we aim to integrate Cardano Smart Contracts by December. Listing is{" "}
-      <b>FREE*</b> but costs <b>~2₳</b> : <b>1₳</b> pegged to your NFT and{" "}
-      <b>~1₳</b> Transactional Fee. Upon successful sale, we charge a <b>3%</b>{" "}
-      fee, with a minimum of <b>2₳</b> for each transaction. This fee comprises:
-      Service Fees <b>(1₳,1%)</b> and Social Donations <b>(1₳,1%)</b>. If you
-      are a successful buyer/seller, head over to the profile section to choose
-      one of the 3 UN Goals to support : Zero Hunger, Climate Action or Quality
-      Education
+      <b>FREE*</b>. You just need to pay <b>~2₳</b> : <b>1₳</b> pegged to your
+      NFT and <b>~1₳</b> for transactional Fee. Upon successful sale, we charge
+      a <b>3%</b> fee, with a minimum of <b>2₳</b> for each transaction. This
+      fee comprises: Service Fees <b>(1₳,1%)</b> and Social Donations{" "}
+      <b>(1₳,1%)</b>. If you are a successful buyer/seller, head over to the
+      profile section to choose one of the 3 UN Goals to support : Zero Hunger,
+      Climate Action or Quality Education
     </p>
   );
 
@@ -81,7 +81,7 @@ const About = () => {
       <p>
         Furthermore, we are offering <b>0% fees</b>! As a reward, delegators who
         stake a minimum of <b>1000 ADA</b> will receive our very first,{" "}
-        <b>exclusive utility NFT drop</b> by the end of the year! More
+        <b>exclusive utility NFT drop</b> by the start of next year! More
         information will be released soon.
       </p>
     </p>
