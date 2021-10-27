c7b99583a6180979761467d3174d1ae54c7a2fff
Elton <57287843+eltontay@users.noreply.github.com>
2021-10-27 21:23:27 +0800

    Css changes to staking pool

--- frontend/src/pages/staking-pool.tsx
+++ frontend/src/pages/staking-pool.tsx
@@ -8,7 +8,7 @@ const ComingSoon = () => {
     <Main>
       <div>
         <div className="flex justify-between w-full min-h-full overflow-hidden bg-bottom bg-no-repeat bg-cover min-w-1200 bg-primary-default bg-background-seascape">
-          <div className="z-10 w-2/5 pl-8 pr-2 md:ml-14 2xl:ml-56 my-44 max-w-maxScreen">
+          <div className="z-10 w-2/5 px-8 md:ml-14 2xl:ml-56 my-44 max-w-maxScreen">
             <p className="mb-16 text-4xl font-bold leading-snug break-words sm:break-normal md:text-5xl lg:text-6xl">
               Staking Pool [WOT]
             </p>
@@ -33,30 +33,32 @@ const ComingSoon = () => {
             className="object-contain"
           />
         </div>
-        <div className="flex flex-col items-center p-16">
-          <div className="flex justify-center">
-            <iframe
-              src="https://stakada.io/embed/stats?pool_id=e00dac4566812442174bcfb4c12225f7a305606eed12f62e62085253&credits=0"
-              width="100%"
-              height="400"
-              scrolling="no"
-            ></iframe>
+        <div className="w-full px-8 md:px-20 2xl:px-56 2xl:max-w-maxBody 2xl:m-auto">
+          <div className="flex flex-col items-center pt-6">
+            <div className="flex justify-center">
+              <iframe
+                src="https://stakada.io/embed/stats?pool_id=e00dac4566812442174bcfb4c12225f7a305606eed12f62e62085253&credits=0"
+                width="100%"
+                height="400"
+                scrolling="no"
+              ></iframe>
+            </div>
           </div>
-        </div>
-        <iframe
-          src="https://stakada.io/embed/howto?pool_id=e00dac4566812442174bcfb4c12225f7a305606eed12f62e62085253&binance_ref=20353988"
-          width="100%"
-          height="600"
-          scrolling="no"
-        ></iframe>
-        <div className="flex flex-col items-center p-16">
-          <div className="flex justify-center">
-            <iframe
-              src="https://stakada.io/embed/epoch"
-              width="100%"
-              height="100"
-              className="max-width:400px;"
-            ></iframe>
+          <iframe
+            src="https://stakada.io/embed/howto?pool_id=e00dac4566812442174bcfb4c12225f7a305606eed12f62e62085253&binance_ref=20353988"
+            width="100%"
+            height="600"
+            scrolling="no"
+          ></iframe>
+          <div className="flex flex-col items-center pt-6">
+            <div className="flex justify-center">
+              <iframe
+                src="https://stakada.io/embed/epoch"
+                width="100%"
+                height="100"
+                className="max-width:400px;"
+              ></iframe>
+            </div>
           </div>
         </div>
       </div>
