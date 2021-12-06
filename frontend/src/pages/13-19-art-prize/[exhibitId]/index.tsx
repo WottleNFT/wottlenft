4bc73dd67fbd4364b0756431f8782685c8b1a7df
dominiclet <dominic.let@hotmail.com>
2021-12-06 12:43:09 +0800

    Update headers and header art

--- frontend/src/pages/13-19-art-prize/[exhibitId]/index.tsx
+++ frontend/src/pages/13-19-art-prize/[exhibitId]/index.tsx
@@ -7,13 +7,13 @@ import { Main } from "../../../templates/Main";
 const Exhibit = () => {
   const imageUrl = "https://placekitten.com/400/400";
   const title = "Title";
-  const author = "Author name here";
+  const author = "[Author name here]";
   const description =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
 
   return (
     <Main>
-      <div className="flex flex-col items-center px-4 pb-20 md:px-12 2xl:px-52">
+      <div className="flex flex-col items-center px-4 pb-20 md:px-12 2xl:px-52 bg-primary-default">
         <div className="relative w-full m-4 h-70vh min-h-320">
           <Image
             className="rounded-2xl"
@@ -29,9 +29,7 @@ const Exhibit = () => {
               <p className="w-full text-2xl font-bold truncate whitespace-normal">
                 {title}
               </p>
-              <span>
-                By <span className="text-primary-default">{author}</span>
-              </span>
+              <span>By {author}</span>
               <p className="mt-4 text-base whitespace-normal">{description}</p>
             </div>
           </div>
