a3034a66ba7b1b1d0a89368f59f065bfeba5d548
Elton <57287843+eltontay@users.noreply.github.com>
2021-12-09 15:36:56 +0800

    adding frontend to drop

--- frontend/src/Components/NftDrop/DropCountdownCard.tsx
+++ frontend/src/Components/NftDrop/DropCountdownCard.tsx
@@ -1,15 +1,15 @@
-import { IonCard } from "@ionic/react";
-import Image from "next/image";
-import { useRouter } from "next/router";
+import { IonButton, IonCard } from "@ionic/react";
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
-import styles from "../../styles/nftDrop.module.css";
-import CountdownTimer from "../Auctions/CountdownTimer";
+import styles from '../../styles/nftDrop.module.css';
+import CountdownTimer from '../Auctions/CountdownTimer';
 
 type Props = {
   banner: StaticImageData;
   countdownTo: number;
   nameLink: string;
-}
+};
 
 const DropCountdownCard = ({ banner, countdownTo, nameLink }: Props) => {
   const router = useRouter();
@@ -27,14 +27,21 @@ const DropCountdownCard = ({ banner, countdownTo, nameLink }: Props) => {
           src={banner}
         />
       </div>
-      <div>
-        14 December 2021
-        
+      <div className="flex flex-row grid-cols-2">
+        <div className="flex flex-col">
+          <div className="flex">Cardanorcs</div>
+          <div className="flex">Launch Date : 14 December 2021</div>
+          <div className="flex"> Supply : 5000 Unique Cardanorcs</div>
+          <div className="flex"> Price : 35 ₳</div>
+        </div>
+        <div className="flex flex-col justify-end justify-items-end">
+        <CountdownTimer
+          countdownTimestamp={countdownTo}
+          className="flex justify-end w-full text-lg font-bold leading-snug text-black sm:leading-normal sm:text-2xl md:text-4xl lg:text-5xl xl:text-6xl"
+        />
+        <IonButton size="default" className="flex justify-end">View</IonButton>
+        </div>
       </div>
-      <CountdownTimer
-        countdownTimestamp={countdownTo}
-        className="absolute bottom-0 w-full text-lg font-bold leading-snug text-center text-black sm:leading-normal sm:text-2xl md:text-4xl lg:text-5xl xl:text-6xl"
-      />
     </IonCard>
   );
 };
