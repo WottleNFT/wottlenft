1c2ca9d7c5fc628b7756fe005b7743fe0853b7a7
dominiclet <dominic.let@hotmail.com>
2021-12-10 12:07:40 +0800

    Update design for NFT drop card

--- frontend/src/Components/NftDrop/DropCountdownCard.tsx
+++ frontend/src/Components/NftDrop/DropCountdownCard.tsx
@@ -1,48 +1,61 @@
-import { IonButton, IonCard } from '@ionic/react';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { IonButton, IonCard } from "@ionic/react";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
-import styles from '../../styles/nftDrop.module.css';
-import CountdownTimer from '../Auctions/CountdownTimer';
+import CountdownTimer from "../Auctions/CountdownTimer";
 
 type Props = {
   banner: StaticImageData;
   countdownTo: number;
   nameLink: string;
+  showViewButton: boolean;
+  launch: string;
+  supply: string;
+  price: number;
 };
 
-const DropCountdownCard = ({ banner, countdownTo, nameLink }: Props) => {
+const DropCountdownCard = ({
+  banner,
+  countdownTo,
+  nameLink,
+  showViewButton,
+  launch,
+  supply,
+  price,
+}: Props) => {
   const router = useRouter();
 
   return (
-    <IonCard
-      className={styles.nftDropBanner}
-      onClick={() => router.push(`/nft-drops/${nameLink}`)}
-    >
-      <div className={styles.innerBanner}>
-        <Image
-          layout="fill"
-          alt="Cardanorc banner"
-          objectFit="cover"
-          src={banner}
-        />
+    <IonCard className="w-full rounded-2xl h-smBanner md:w-mdBanner md:h-mdBanner lg:h-lgBanner lg:w-lgBanner xl:h-xlBanner xl:w-xlBanner">
+      <div className="relative w-full h-3/5 lg:h-4/6">
+        <Image layout="fill" alt="Drop banner" objectFit="cover" src={banner} />
       </div>
-      <div className="flex flex-row grid-cols-2 min-h-full ">
-        <div className="flex flex-col w-1/2 ml-8">
-          <div className="flex">
-            <p className="font-bold text-black text-lg">Cardanorcs</p></div>
-          <div className="flex">Launch Date : 14 December 2021</div>
-          <div className="flex"> Supply : 5000 Unique Cardanorcs</div>
-          <div className="flex"> Price : 35 ₳</div>
+      <div className="flex flex-row items-center justify-between grid-cols-2 p-2 md:px-4 md:py-2 xl:py-4">
+        <div className="flex flex-col">
+          <p className="text-xl font-bold text-black md:text-3xl lg:text-4xl xl:text-5xl">
+            Cardanorcs
+          </p>
+          <p className="text-xs leading-tight text-black md:text-sm lg:text-base">
+            <b>Launch Date :</b> {launch}
+          </p>
+          <p className="hidden text-sm leading-tight text-black md:block lg:text-base">
+            <b>Supply :</b> {supply} <br />
+            <b>Price :</b> {price} ₳
+          </p>
         </div>
-        <div className="flex flex-col justify-items-end">
+        <div className="flex flex-col justify-items-center">
           <CountdownTimer
             countdownTimestamp={countdownTo}
-            className="flex w-full text-lg font-bold leading-snug text-black sm:leading-normal sm:text-2xl md:text-4xl lg:text-5xl xl:text-6xl"
+            className="flex w-full font-bold leading-snug text-center text-black text-md sm:leading-normal sm:text-2xl md:text-4xl lg:text-5xl"
           />
-          <IonButton size="default" className="flex ">
-            View
-          </IonButton>
+          {showViewButton && (
+            <IonButton
+              size="default"
+              onClick={() => router.push(`/nft-drops/${nameLink}`)}
+            >
+              View
+            </IonButton>
+          )}
         </div>
       </div>
     </IonCard>
--- frontend/src/pages/nft-drops/index.tsx
+++ frontend/src/pages/nft-drops/index.tsx
@@ -10,11 +10,15 @@ const NFTDrops = () => {
           <p className="w-full px-8 text-3xl font-bold leading-snug break-words 2xl:px-40 sm:break-normal md:text-4xl lg:text-5xl ">
             NFT DROPS
           </p>
-          <div className="flex flex-col items-center w-full px-8 py-5 2xl:px-40">
+          <div className="flex flex-col items-center w-full px-1 py-5 sm:px-8 2xl:px-40">
             <DropCountdownCard
               banner={cardanorcBanner}
               countdownTo={1639454400}
               nameLink="cardanorcs"
+              showViewButton={true}
+              launch="14 December 2021, 4 A.M. UTC"
+              supply="5000 Unique Cardanorcs"
+              price={35}
             />
           </div>
         </div>
