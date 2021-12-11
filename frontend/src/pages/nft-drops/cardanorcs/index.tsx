5a2f57b8f9951b5e40ee224132e66a705373b1ad
Elton <57287843+eltontay@users.noreply.github.com>
2021-12-11 05:03:18 +0100

    add responsiveness to nft drop

--- frontend/src/pages/nft-drops/cardanorcs/index.tsx
+++ frontend/src/pages/nft-drops/cardanorcs/index.tsx
@@ -14,8 +14,8 @@ const Cardanorcs = () => {
   return (
     <Main title="NFT Drops | Cardanorcs">
       <div className="bg-primary-default">
-        <div className="flex flex-col items-center w-full min-h-full px-2 py-10 m-auto md:px-10 max-w-maxBody">
-          <div className="flex flex-row self-start justify-between 2xl:ml-32">
+        <div className="flex flex-col items-center w-full min-h-full px-12 2xl:px-40 py-10 max-w-maxBody">
+          <div className=" flex flex-row justify-between lg:w-lgBanner xl:w-xlBanner md:w-mdBanner">
             <div className="flex flex-col md:max-w-maxHalf">
               <p className="text-5xl font-bold leading-snug break-words sm:break-normal md:text-6xl lg:text-7xl">
                 NFT DROPS
@@ -30,11 +30,6 @@ const Cardanorcs = () => {
                 endless struggle for survival. Who are these creatures and will
                 their emergence from the void disrupt Cardano?
               </p>
-              <Link href="https://cardanorcs.com/">
-                <a className="w-32 mt-5 text-2xl text-blue-700 hover:underline">
-                  cardanorcs.com
-                </a>
-              </Link>
             </div>
             <div
               className="relative hidden md:block"
@@ -48,21 +43,30 @@ const Cardanorcs = () => {
               />
             </div>
           </div>
-          <div className="flex justify-end w-full gap-4 text-2xl lg:text-4xl lg:w-lgBanner xl:w-xlBanner md:w-mdBanner">
-            <a
-              target="_blank"
-              href="https://twitter.com/cardanorcs"
-              rel="noreferrer"
-            >
-              <BsTwitter />
-            </a>
-            <a
-              target="_blank"
-              href="https://discord.gg/xs32ewMWBf"
-              rel="noreferrer"
-            >
-              <FaDiscord />
-            </a>
+          <div className="flex pt-4 w-full flex-row lg:text-4xl lg:w-lgBanner xl:w-xlBanner md:w-mdBanner">
+            <div className="flex justify-start w-1/2 text-2xl lg:text-4xl ">
+              <Link href="https://cardanorcs.com/">
+                <a className="w-32 text-2xl text-blue-700 hover:underline">
+                  cardanorcs.com
+                </a>
+              </Link>
+            </div>
+            <div className=" flex justify-end w-1/2 gap-4 text-2xl lg:text-4xl ">
+              <a
+                target="_blank"
+                href="https://twitter.com/cardanorcs"
+                rel="noreferrer"
+              >
+                <BsTwitter />
+              </a>
+              <a
+                target="_blank"
+                href="https://discord.gg/xs32ewMWBf"
+                rel="noreferrer"
+              >
+                <FaDiscord />
+              </a>
+            </div>
           </div>
           <DropCountdownCard
             banner={cardanorcBanner}
