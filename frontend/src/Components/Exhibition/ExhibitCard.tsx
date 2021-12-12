4bcca892090d68af56cacdeb80f454fd28ec44d3
dominiclet <dominic.let@hotmail.com>
2021-12-12 18:29:46 +0800

    Update TRCL exhibition card to link to TRCL auction page

--- frontend/src/Components/Exhibition/ExhibitCard.tsx
+++ frontend/src/Components/Exhibition/ExhibitCard.tsx
@@ -1,6 +1,5 @@
 import { IonCard, IonCardHeader, IonCardTitle } from "@ionic/react";
 import Image from "next/image";
-import Link from "next/link";
 
 interface Props {
   imageUrl: string;
@@ -12,19 +11,21 @@ const ExhibitCard = ({ imageUrl, description, author }: Props) => {
   return (
     <IonCard className="px-2 pt-3 m-0 rounded-3xl md:px-4">
       <div className="flex flex-col">
-        <Link href="/TRCL-13-19-art-prize/1">
-          <a>
-            <div className="relative w-full aspect-w-1 aspect-h-1">
-              <Image
-                className="rounded-2xl"
-                alt="NFT Image"
-                layout="fill"
-                objectFit="cover"
-                src={imageUrl}
-              />
-            </div>
-          </a>
-        </Link>
+        <a
+          href="https://trcl.sg/13-19-art-prize/auction/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="relative w-full aspect-w-1 aspect-h-1">
+            <Image
+              className="rounded-2xl"
+              alt="NFT Image"
+              layout="fill"
+              objectFit="cover"
+              src={imageUrl}
+            />
+          </div>
+        </a>
         <div className="flex flex-col justify-center w-full h-1/3">
           <IonCardHeader className="px-2 truncate">
             <IonCardTitle className="text-base text-center truncate">
